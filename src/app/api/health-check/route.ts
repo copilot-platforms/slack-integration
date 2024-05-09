0421462284bda25a7ca273284dbe75710cd4890d
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-09 07:43:30 +0545

    feat: check db connection on health-check endpoint

--- src/app/api/health-check/route.ts
+++ src/app/api/health-check/route.ts
@@ -1,7 +1,20 @@
+import DBClient from '@/lib/db';
+import httpStatus from 'http-status';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({
-    message: 'Working fine!',
-  });
+  try {
+    await DBClient.getInstance().$queryRaw`SELECT 1`;
+    return NextResponse.json({
+      message: '🔥 Slack Integration is rolling 🔥',
+    });
+  } catch (error: unknown) {
+    return NextResponse.json(
+      {
+        message: '💥 Houston, we have a problem 💥',
+        error,
+      },
+      { status: httpStatus.INTERNAL_SERVER_ERROR },
+    );
+  }
 }
