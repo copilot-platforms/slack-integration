4fadc9542f65dcc31b32304add924755e9232acc
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-10 12:45:20 +0545

    feat: add settings table schema

---
+++ prisma/migrations/20240510061338_add_settings_table/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "ChannelSyncOptions" AS ENUM ('clientAndCompany');
+
+-- CreateTable
+CREATE TABLE "Settings" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "workspaceId" VARCHAR(32) NOT NULL,
+    "bidirectionalSlackSync" BOOLEAN NOT NULL DEFAULT false,
+    "channelsToSync" "ChannelSyncOptions" NOT NULL DEFAULT 'clientAndCompany',
+    "fallbackMessageSenderId" UUID NOT NULL,
+    "slackChannelPrefix" VARCHAR(255) NOT NULL,
+    "isSyncing" BOOLEAN NOT NULL DEFAULT false,
+    "syncedById" UUID,
+    "lastSyncedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Settings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "IX_Settings_workspaceId_deletedAt" ON "Settings"("workspaceId", "deletedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Settings_workspaceId_deletedAt_key" ON "Settings"("workspaceId", "deletedAt");
