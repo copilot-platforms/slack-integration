6d2785fecdc3b20880598d8e669f1f982045242c
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-13 11:25:44 +0545

    fix: don't allow spaces in slack channel prefix

--- src/types/dtos/settings.dto.ts
+++ src/types/dtos/settings.dto.ts
@@ -5,7 +5,7 @@ export const CreateUpdateSettingsSchema = z.object({
   bidirectionalSlackSync: z.boolean(),
   channelsToSync: z.nativeEnum(ChannelSyncOptions),
   fallbackMessageSenderId: z.string().uuid(),
-  slackChannelPrefix: z.string().min(1).max(255),
+  slackChannelPrefix: z.string().min(1).max(255).regex(/^\S+$/, 'Slack channel prefix must not contain spaces'),
   isSyncing: z.boolean(),
 })
 export type CreateUpdateSettingsDTO = z.infer<typeof CreateUpdateSettingsSchema>
