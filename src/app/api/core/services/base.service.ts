e60a928544182b77ef51d5fd5d154f9230ab3bc5
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-29 00:52:21 +0545

    chore: inject copilot sdk to base service

--- src/app/api/core/services/base.service.ts
+++ src/app/api/core/services/base.service.ts
@@ -1,12 +1,16 @@
 import DBClient from '@/lib/db'
 import { PrismaClient } from '@prisma/client'
 import User from '@api/core/models/User.model'
+import { CopilotAPI } from '@/utils/CopilotAPI'
 
 /**
  * Base Service with access to db and current user
  */
 export abstract class BaseService {
   protected db: PrismaClient = DBClient.getInstance()
+  protected copilot: CopilotAPI
 
-  constructor(public user: User) {}
+  constructor(protected user: User) {
+    this.copilot = new CopilotAPI(user.token)
+  }
 }
