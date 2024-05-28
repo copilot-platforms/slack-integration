b3d93554ffc100591dba34c8951afb009d60a37e
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-29 00:54:11 +0545

    feat: add zeplo env config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,10 @@ export const apiUrl = `${process.env.VERCEL_ENV === 'development' ? 'http://' : 
 export const SentryConfig = {
   DSN: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
 }
+export const vercelConfig = {
+  env: process.env.VERCEL_ENV || 'development',
+}
+export const zeploConfig = {
+  apiKey: process.env.ZEPLO_API_KEY || '',
+  url: vercelConfig.env === 'development' ? process.env.ZEPLO_DEV_URL : apiUrl,
+}
