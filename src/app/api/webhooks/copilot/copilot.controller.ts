06299eec0241e9b483e6410de1de51fec02233fa
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-29 00:55:11 +0545

    feat: add webhooks endpoint for copilot

--- src/app/api/webhooks/copilot/copilot.controller.ts
+++ src/app/api/webhooks/copilot/copilot.controller.ts
@@ -2,22 +2,23 @@ import { NextRequest, NextResponse } from 'next/server'
 import User from '@/app/api/core/models/User.model'
 import { CopilotWebhookService } from '@api/webhooks/copilot/copilot.service'
 import { SettingsService } from '@api/settings/settings.service'
+import { WebhookSchema } from '@api/core/types/webhook'
 
 export const handleCopilotWebhookEvent = async (req: NextRequest) => {
+  // Authenticates if the webhook comes from a valid source - no signature matching is necessary
   const user = await User.authenticate(req)
 
   // If sync is not running for a workspace, we can safely ignore this webhook call
   const settingsService = new SettingsService(user)
   const settings = await settingsService.getSettings()
   if (!settings?.isSyncRunning) {
+    console.info('Sync not running, ignoring webhook call')
     return NextResponse.json({ message: 'Sync not running, ignoring webhook call' })
   }
 
-  const body = await req.json()
-  const copilotWebhookService = new CopilotWebhookService(user, settings)
-  // Verify that webhook was called by copilot app using the `x-copilot-signature` header
-  const data = await copilotWebhookService.verifySignature(body, req.headers.get('x-copilot-signature'))
   // Handle webhook based on event that triggered it
+  const data = WebhookSchema.parse(await req.json())
+  const copilotWebhookService = new CopilotWebhookService(user, settings)
   await copilotWebhookService.handleWebhookEvent(data)
 
   return NextResponse.json({
--- src/app/api/webhooks/copilot/copilot.service.ts
+++ src/app/api/webhooks/copilot/copilot.service.ts
@@ -1,20 +1,20 @@
 import httpStatus from 'http-status'
+import { Setting } from '@prisma/client'
+import { ChannelResponse } from '@/types/common'
 import { CopilotAPI } from '@/utils/CopilotAPI'
-import { WebhooksService } from '@api/webhooks/webhooks.service'
+import { kebabify } from '@/utils/string'
 import { WebhookActions, WebhookEvent } from '@api/core/types/webhook'
 import { ChannelSchema } from '@api/core/types/message'
 import APIError from '@api/core/exceptions/APIError'
 import { parseUserIdAndEmail } from '@api/core/utils/users'
-import { SlackbotService } from '../../core/services/slackbot.service'
-import { Setting } from '@prisma/client'
+import { RequestQueueService } from '@api/core/services/requestQueue.service'
+import { BaseService } from '@api/core/services/base.service'
 import User from '@api/core/models/User.model'
-import { ChannelResponse } from '@/types/common'
-import { kebabify } from '@/utils/string'
 
-export class CopilotWebhookService extends WebhooksService {
+export class CopilotWebhookService extends BaseService {
   constructor(
     user: User,
-    public syncSettings: Setting,
+    public readonly settings: Setting,
   ) {
     super(user)
   }
@@ -29,63 +29,110 @@ export class CopilotWebhookService extends WebhooksService {
       'messageChannel.deleted': this.handleChannelDeleted,
       'message.sent': this.handleMessageSent,
     }
-    // Fetch  and run appropriate action, if exists, else ignore this webhook event
+    // Fetch  and run appropriate action if event type exists in webhookActions keys, else ignore this webhook event
     webhookActions[data.eventType as keyof WebhookActions]?.(data)
   }
-
-  // TODO: implement in relavant PR
-  private handleMessageSent(_: WebhookEvent) {}
 
   /**
    * Handle webhook envent for `messageChannel.created` from Copilot Messages app
    * @param data Parsed WebhookEvent related to this event
    */
-  private async handleChannelCreated(data: WebhookEvent) {
+  private handleChannelCreated = async (data: WebhookEvent) => {
+    // Extract newly created channel info from webhook payload
     const channelInfo = ChannelSchema.parse(data.data)
 
+    // Get relavant info to sync this channel to Slack
     const copilot = new CopilotAPI(this.user.token)
-    // Get newly created channel info
     const channel = await copilot.getMessageChannel(channelInfo.id)
+    const targetName = await this.getTargetName(channel)
+    const emails = await this.getChannelParticipantEmails(channel)
+    const channelName = await this.getChannelName(targetName)
 
+    // Create channel sync record in SyncedChannels table
+    const sync = await this.db.syncedChannel.create({
+      data: {
+        copilotChannelId: channel.id,
+        slackChannelName: channelName,
+        status: 'pending',
+      },
+    })
+
+    // Create a new Slack channel and send invites to all associated emails
+    const requestQueue = new RequestQueueService()
+    requestQueue.push('/api/workers/copilot', {
+      traceId: sync.id,
+      params: [{ channelName, emails }],
+    })
+  }
+
+  private handleChannelDeleted = (data: WebhookEvent) => {
+    // TODO: After implementing slack bot
+  }
+
+  private handleMessageSent = (_: WebhookEvent) => {
+    // TODO: implement in relavant PR
+  }
+
+  /**
+   * Get new unique slack channel name in format prefix-client-name or prefix-company-name
+   * @param targetName Target client's full name or target company's name
+   * @returns Unique kebab cased Slack channel name
+   */
+  private getChannelName = async (targetName: string): Promise<string> => {
+    let defaultName = `${this.settings.slackChannelPrefix}-${kebabify(targetName)}`
+    // If name already exists then append a `-2` kind of suffix to make it unique
+    const channelNameOccurances = await this.db.syncedChannel.count({
+      where: { slackChannelName: { startsWith: defaultName } },
+    })
+    if (channelNameOccurances) {
+      defaultName += `-${String(channelNameOccurances + 1)}`
+    }
+
+    return defaultName
+  }
+
+  /**
+   * Get a channel's target client's full name or target company's name
+   * @param channel Relavant channel
+   * @returns Name of the target client / company
+   */
+  private getTargetName = async (channel: ChannelResponse): Promise<string> => {
     // Get the client / company that is the target of this message channel
     let targetName: string
+
     if (channel.membershipType === 'individual') {
-      const target = await copilot.getClient(channel.membershipEntityId)
+      // Client ID is the one that is not present in internalUserIds array - so calculate difference of arrays
+      // This will always result in an array with single value of the client ID for membershipType 'individual'
+      const internalUserIds = (await this.copilot.getInternalUsers()).data.map((user) => user.id)
+      // This is being done because membershipEntityId doesn't always return ID of client
+      const clientId = channel.memberIds.filter((id) => !internalUserIds.includes(id))
+      const target = await this.copilot.getClient(clientId[0])
       targetName = `${target.givenName} ${target.familyName}`
     } else if (channel.membershipType === 'company') {
-      const target = await copilot.getCompany(channel.membershipEntityId)
+      const target = await this.copilot.getCompany(channel.membershipEntityId)
       targetName = target.name
     } else {
       throw new APIError(httpStatus.INTERNAL_SERVER_ERROR, 'Group messages are not supported yet by Copilot')
     }
 
+    return targetName
+  }
+
+  /**
+   * Gets an array of channel participant's emails
+   * @param channel Relavant channel
+   * @returns Array of participant emails
+   */
+  private getChannelParticipantEmails = async (channel: ChannelResponse): Promise<string[]> => {
     // Get an array of emails of each message channel participant
-    const internalUsers = parseUserIdAndEmail((await copilot.getInternalUsers()).data)
-    const clients = parseUserIdAndEmail((await copilot.getClients()).data)
+    const internalUsers = parseUserIdAndEmail((await this.copilot.getInternalUsers()).data)
+    const clients = parseUserIdAndEmail((await this.copilot.getClients()).data)
     const emails: string[] = []
     channel.memberIds.forEach((memberId) => {
       // Fetch email whether it is of an internal user or a client
       const email = internalUsers[memberId] ?? clients[memberId]
       email && emails.push(email)
     })
-
-    // Create a single new channel and send invites to all associated emails
-
-    const slackbot = new SlackbotService()
-    await this.bulkCreateChannels([
-      {
-        channelName: `${this.syncSettings.slackChannelPrefix}-${kebabify(targetName)}`,
-        emails,
-      },
-    ])
-
-    // Create channel sync record in SyncedChannels table
+    return emails
   }
-
-  private handleChannelDeleted(data: WebhookEvent) {
-    // TODO: After implementing slack bot
-    console.log('Post channel was deleted in integrated slack channel')
-  }
-
-  private bulkCreateChannels(channels: { channelName: string; emails: string[] }[]) {}
 }
