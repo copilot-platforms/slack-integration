778ae85e0a24a1ab877f2fff335b84a217c8df0a
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-29 00:52:04 +0545

    feat: add synced channels table schema

--- prisma/migrations/20240528184541_add_synced_channels_table/migration.sql
+++ prisma/migrations/20240528184541_add_synced_channels_table/migration.sql
@@ -1,10 +1,13 @@
+-- CreateEnum
+CREATE TYPE "SyncStatus" AS ENUM ('pending', 'success', 'failed');
+
 -- CreateTable
 CREATE TABLE "SyncedChannels" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "copilotChannelId" VARCHAR(32) NOT NULL,
-    "slackChannelId" VARCHAR(9) NOT NULL,
-    "slackChannelName" VARCHAR(255) NOT NULL,
-    "syncedById" UUID NOT NULL,
+    "copilotChannelId" VARCHAR(32),
+    "slackChannelId" VARCHAR(9),
+    "slackChannelName" VARCHAR(255),
+    "status" "SyncStatus" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "SyncedChannels_pkey" PRIMARY KEY ("id")
@@ -15,6 +18,9 @@ CREATE INDEX "IX_SyncedChannels_slackChannelId" ON "SyncedChannels"("slackChanne
 
 -- CreateIndex
 CREATE INDEX "IX_SyncedChannels_copilotChannelId" ON "SyncedChannels"("copilotChannelId");
+
+-- CreateIndex
+CREATE INDEX "IX_SyncedChannels_status_copilotChannelId" ON "SyncedChannels"("status", "copilotChannelId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "SyncedChannels_slackChannelId_key" ON "SyncedChannels"("slackChannelId");
