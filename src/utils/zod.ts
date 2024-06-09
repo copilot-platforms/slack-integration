da59b9cd857ee678372a421acb73d733e798423e
Rojan Rajbhandari <rrojan20@gmail.com>
2024-06-09 20:29:41 +0545

    chore: add util to build pretty zod errors

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -1,15 +1,15 @@
+import { ZodError } from 'zod'
+
 /**
- * Util to build a proper user readable error message from ZodError.errors object
- * @param errors Array from ZodError.errors
- * @param key Key / fieldname to query errors for
- * @returns User readable error message
+ * Format errors properly - by default zod fieldErrors is a string array joined together by nothing which looks ugly
  */
-
-import { ZodFormattedError } from 'zod'
-
-export const getFirstErrorMessage = <T extends Record<string, unknown>>(
-  errors: ZodFormattedError<T, string>,
-  key: keyof T,
-): string | undefined => {
-  return errors?.[key]?._errors?.[0]?.replace('String', 'Field')
+export const buildFormattedFieldErrors = (error: ZodError) => {
+  const errors = error.formErrors.fieldErrors
+  let formattedErrors: { [x: string | number | symbol]: string | undefined } = {}
+  Object.keys(errors).forEach((field) => {
+    if (errors[field]) {
+      formattedErrors[field] = errors[field]?.join(', ')
+    }
+  })
+  return formattedErrors
 }
