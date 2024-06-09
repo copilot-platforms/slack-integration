b1459fffd3f03ebc1bdb00d37e5a1b0097b46378
Rojan Rajbhandari <rrojan20@gmail.com>
2024-06-09 20:29:27 +0545

    feat: add loading state to buttons

--- src/components/Buttons/PrimaryBtn.tsx
+++ src/components/Buttons/PrimaryBtn.tsx
@@ -1,15 +1,16 @@
 'use client'
 
-import { Button, Typography } from '@mui/material'
+import { Button, CircularProgress, Typography } from '@mui/material'
 
 interface PrimaryBtnProps {
   type?: 'button' | 'reset' | 'submit'
   handleClick?: () => void
+  isLoading?: boolean
   disabled?: boolean
   children?: string
 }
 
-export const PrimaryBtn = ({ type, handleClick, disabled = false, children }: PrimaryBtnProps) => {
+export const PrimaryBtn = ({ type, handleClick, isLoading, disabled = false, children }: PrimaryBtnProps) => {
   return (
     <Button
       variant="contained"
@@ -22,6 +23,7 @@ export const PrimaryBtn = ({ type, handleClick, disabled = false, children }: Pr
       type={type ?? 'button'}
       disabled={disabled}
     >
+      {isLoading && <CircularProgress size={12} color="inherit" sx={{ marginRight: '0.5em' }} />}
       <Typography variant="sm">{children}</Typography>
     </Button>
   )
