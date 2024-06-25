3abdf51f4cb24dcf568ee78e1041743df519e726
Rojan Rajbhandari <rrojan20@gmail.com>
2024-06-25 21:47:04 +0545

    feat: sync fix

--- src/app/api/slack/install/route.ts
+++ src/app/api/slack/install/route.ts
@@ -1,12 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { slackConfig } from '@/config'
 import User from '@api/core/models/User.model'
+import { SyncedWorkspacesService } from '@api/synced-workspaces/synced-workspaces.service'
 
 export const GET = async (req: NextRequest) => {
   const user = await User.authenticate(req)
 
   const { clientId, redirectUri } = slackConfig
+  const syncedWorkspacesService = new SyncedWorkspacesService(new User(slackConfig.validToken, null as any))
+  await syncedWorkspacesService.addAsSynced(user.workspaceId)
   return NextResponse.redirect(
-    `https://slack.com/oauth/v2/authorize?client_id=${clientId}&scope=chat:write,groups:write,channels:manage,groups:write.invites&redirect_uri=${redirectUri}?workspaceId=${user.workspaceId}`,
+    `https://slack.com/oauth/v2/authorize?client_id=${clientId}&scope=chat:write,groups:write,channels:manage,groups:write.invites&redirect_uri=${redirectUri}`,
   )
 }
--- src/app/api/slack/oauth-redirect/route.ts
+++ src/app/api/slack/oauth-redirect/route.ts
@@ -3,12 +3,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import { apiUrl, slackConfig } from '@/config'
 import APIError from '@api/core/exceptions/APIError'
 import httpStatus from 'http-status'
-import { SyncedWorkspacesService } from '../../synced-workspaces/synced-workspaces.service'
-import User from '@api/core/models/User.model'
-import { z } from 'zod'
 
 export const GET = async (req: NextRequest) => {
-  const workspaceId = z.string().parse(req.nextUrl.searchParams.get('workspaceId'))
   const slackClient = new WebClient()
   const code = req.nextUrl.searchParams.get('code')
 
@@ -28,8 +24,7 @@ export const GET = async (req: NextRequest) => {
 
     // Store the access token and team ID in your database
     console.info('Authenticated team', team, 'with access token:', access_token)
-    const syncedWorkspacesService = new SyncedWorkspacesService(new User(slackConfig.validToken, null as any))
-    await syncedWorkspacesService.addAsSynced(workspaceId)
+
     return NextResponse.redirect(`${apiUrl}/oauth-success`)
   } catch (error) {
     throw new APIError(httpStatus.BAD_REQUEST, 'Error during OAuth verification')
