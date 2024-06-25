ccae5e909f90549664517c49bbc904cf285fc5ec
Rojan Rajbhandari <rrojan20@gmail.com>
2024-06-25 17:32:30 +0545

    chore: respond with challenge value for slack event subscription

--- src/app/api/webhooks/slack/slack.controller.ts
+++ src/app/api/webhooks/slack/slack.controller.ts
@@ -5,15 +5,16 @@ import User from '@api/core/models/User.model'
 import { slackConfig } from '@/config'
 
 export const handleSlackWebhookRequest = async (req: NextRequest) => {
-  const body = SlackEventSubscriptionSchema.safeParse(await req.json())
-  if (!body.success) {
-    // This is not a relavant message for us to handle
-    return NextResponse.json(true)
-  }
+  return NextResponse.json((await req.json()).challenge)
+  // const body = SlackEventSubscriptionSchema.safeParse(await req.json())
+  // if (!body.success) {
+  //   // This is not a relavant message for us to handle
+  //   return NextResponse.json(true)
+  // }
 
-  const { data } = body
-  const user = await User.authenticateToken(slackConfig.validToken)
-  const slackService = new SlackWebhooksService(user)
-  await slackService.pushMessageToCopilot(data)
-  return NextResponse.json((body as any).challenge)
+  // const { data } = body
+  // const user = await User.authenticateToken(slackConfig.validToken)
+  // const slackService = new SlackWebhooksService(user)
+  // await slackService.pushMessageToCopilot(data)
+  // return NextResponse.json(true)
 }
