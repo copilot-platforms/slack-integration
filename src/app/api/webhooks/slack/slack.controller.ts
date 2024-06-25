705582bb1960878428701673c3ba4eb5a0b455cb
Rojan Rajbhandari <rrojan20@gmail.com>
2024-06-25 17:34:09 +0545

    chore: revert respond with challenge value for slack event subscription

--- src/app/api/webhooks/slack/slack.controller.ts
+++ src/app/api/webhooks/slack/slack.controller.ts
@@ -5,16 +5,15 @@ import User from '@api/core/models/User.model'
 import { slackConfig } from '@/config'
 
 export const handleSlackWebhookRequest = async (req: NextRequest) => {
-  return NextResponse.json((await req.json()).challenge)
-  // const body = SlackEventSubscriptionSchema.safeParse(await req.json())
-  // if (!body.success) {
-  //   // This is not a relavant message for us to handle
-  //   return NextResponse.json(true)
-  // }
+  const body = SlackEventSubscriptionSchema.safeParse(await req.json())
+  if (!body.success) {
+    // This is not a relavant message for us to handle
+    return NextResponse.json(true)
+  }
 
-  // const { data } = body
-  // const user = await User.authenticateToken(slackConfig.validToken)
-  // const slackService = new SlackWebhooksService(user)
-  // await slackService.pushMessageToCopilot(data)
-  // return NextResponse.json(true)
+  const { data } = body
+  const user = await User.authenticateToken(slackConfig.validToken)
+  const slackService = new SlackWebhooksService(user)
+  await slackService.pushMessageToCopilot(data)
+  return NextResponse.json(true)
 }
